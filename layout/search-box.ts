2602f40c99bef1ac80c6d0051107542007327b1e
Gregory Quinio <1063530+undefined@users.noreply.github.com>
2022-04-04 09:48:32 +0000

    Push from fixcolors

--- layout/search-box.ts
+++ layout/search-box.ts
@@ -1,50 +1,8 @@
 import { LitElement, css, html } from 'lit';
-import MiniSearch from 'minisearch';
-
-const miniSearch = new MiniSearch({
-  fields: ['title', 'headline', 'body', 'section'], // fields to index for full-text search
-  storeFields: ['title', 'headline', 'body', 'section'], // fields to return with search results
-  searchOptions: {
-    boost: { headline: 3, title: 2, section: 1 },
-    fuzzy: 0.2,
-    prefix: true,
-  },
-});
-
-const docBlocks = new Map();
-function indexDoc(content: string, { file, title, section, base }) {
-  if (docBlocks.has(file)) miniSearch.removeAll(docBlocks.get(file));
-  let heading: RegExpExecArray;
-  const headingRE = /^\n?(#*)\s+(.*)\n\n([^#]*)/gm;
-  const blocks = [];
-  while ((heading = headingRE.exec(content))) {
-    const [, level, headline, body] = heading;
-    let url = base + file;
-    if (level.length > 1) url += `#\${headline.toLowerCase()}`;
-    blocks.push({ id: url, title, section, headline, body });
-  }
-  docBlocks.set(file, blocks);
-  miniSearch.addAll(blocks);
-}
 
 class SearchBox extends LitElement {
-  placeHolder = window.navigator.platform === 'MacIntel' ? '⌘K' : 'Ctrl+K';
-
-  get searchInput(): HTMLInputElement {
-    return this.shadowRoot.querySelector('#search-input');
-  }
-
-  get searchHits(): HTMLUListElement {
-    return this.shadowRoot.querySelector('#search-hits');
-  }
-
-  get searchBox(): HTMLDivElement {
-    return this.shadowRoot.querySelector('#search-box');
-  }
-
-  get searchOverlay(): HTMLDivElement {
-    return this.shadowRoot.querySelector('#search-overlay');
-  }
+  search = null;
+  hits = null;
 
   constructor() {
     super();
@@ -54,16 +12,6 @@ class SearchBox extends LitElement {
         this.updateHits();
       } else if (e.key === 'Escape') this.hideHits();
     });
-  }
-
-  set docs(value: string[]) {
-    const all = value.map((k) => [k + '?raw', k + '?docmeta']);
-    for (const [docContent, docMeta] of all || []) {
-      Promise.all([
-        import(/* @vite-ignore */ docContent),
-        import(/* @vite-ignore */ docMeta),
-      ]).then(([{ default: content }, meta]) => indexDoc(content, meta));
-    }
   }
 
   render() {
@@ -77,7 +25,6 @@ class SearchBox extends LitElement {
             autocorrect="off"
             autocapitalize="off"
             spellcheck="false"
-            :placeholder="placeholder"
             maxlength="64"
             type="search"
             enterkeyhint="go"
@@ -89,45 +36,38 @@ class SearchBox extends LitElement {
           />
         </label>
       </form>
-      <ul id="search-hits"></ul>
+      <ul id="search-hits">
+        ${this.hits === null
+          ? html`<li><i>Type for searching...</i></li>`
+          : this.hits.length === 0
+          ? html`<li><i>No match found</i></li>`
+          : this.hits.map((hit) => html`<li>${this.hitHTML(hit)}</li>`)}
+      </ul>
       <div id="search-overlay" @click=${() => this.hideHits()}></div>
     </div>`;
   }
 
   hitHTML(hit) {
-    return `<a href="${hit.id}">
-    <header>
-      ${hit.headline}
-      <span class="section">
-        ${hit.title ? `<span>${hit.title}</span>` : ''}
-        ${hit.section ? `<span>${hit.section}</span>` : ''}
-      </span>
-    </header>
-    ${hit.body ? `<code>${hit.body}</code>` : ''}
-  </a>`;
+    return html`<a href="${hit.id}">
+      <header>
+        ${hit.headline}
+        <span class="tags">
+          ${hit.title ? `<span>${hit.title}</span>` : ''}
+          ${hit.section ? `<span>${hit.section}</span>` : ''}
+        </span>
+      </header>
+      ${hit.body ? html`<textarea value=${hit.body}></textarea>` : ''}
+    </a>`;
   }
 
   async updateHits(searchValue?: string) {
     if (!searchValue) {
-      this.searchHits.innerHTML = `<li><i>Type for searching...</i></li>`;
+      this.hits = null;
     } else {
-      const hits = await miniSearch.search(searchValue).slice(0, 10);
-      if (hits.length > 0) {
-        let i = 0;
-        for (; i < hits.length; i++) {
-          const hit = this.transform(hits[i]);
-          let searchHitItem = this.searchHits.childNodes[i] as HTMLElement;
-          if (!searchHitItem) {
-            searchHitItem = document.createElement('li') as HTMLLIElement;
-            searchHitItem.innerHTML = this.hitHTML(hit);
-            this.searchHits.appendChild(searchHitItem);
-          } else searchHitItem.innerHTML = this.hitHTML(hit);
-        }
-        for (; i < this.searchHits.childNodes.length; i++)
-          this.searchHits.childNodes[i].remove();
-      } else {
-        this.searchHits.innerHTML = `<li><i>No match found</i></li>`;
-      }
+      this.hits = (await this.search?.(searchValue))
+        .filter(Boolean)
+        .slice(0, 10)
+        .map(this.highlight);
     }
     this.searchOverlay.style.display = 'block';
     this.searchHits.style.display = 'block';
@@ -135,7 +75,7 @@ class SearchBox extends LitElement {
     this.searchInput.placeholder = '';
   }
 
-  transform(hit) {
+  highlight(hit) {
     hit.body = !hit.body
       ? ''
       : hit.body.length < 100
@@ -160,25 +100,42 @@ class SearchBox extends LitElement {
     this.searchInput.placeholder = this.placeHolder;
   }
 
+  get placeHolder() {
+    return window.navigator.platform === 'MacIntel' ? '⌘K' : 'Ctrl+K';
+  }
+
+  get searchInput(): HTMLInputElement {
+    return this.shadowRoot.querySelector('#search-input');
+  }
+
+  get searchHits(): HTMLUListElement {
+    return this.shadowRoot.querySelector('#search-hits');
+  }
+
+  get searchOverlay(): HTMLDivElement {
+    return this.shadowRoot.querySelector('#search-overlay');
+  }
+
   static get styles() {
     return css`
+      :host {
+        --c-bg-color: var(--dockit-layout-bg);
+        --c-border-color: var(--dockit-layout-header-border-color);
+      }
       #search-box {
         position: relative;
         flex-grow: 1;
         margin-left: 1rem;
       }
-
       #search-box form {
         z-index: 11;
         display: flex;
         align-items: center;
       }
-
       #search-box form label {
         position: relative;
         width: 100%;
       }
-
       #search-box form label:before {
         content: '';
         z-index: 12;
@@ -187,35 +144,32 @@ class SearchBox extends LitElement {
         top: 0;
         bottom: 0;
         width: 20px;
-        background: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='32' height='32' viewBox='0 0 20 20' fill-rule='evenodd'%3E%3Cpath d='M14.386 14.386l4.0877 4.0877-4.0877-4.0877c-2.9418 2.9419-7.7115 2.9419-10.6533 0-2.9419-2.9418-2.9419-7.7115 0-10.6533 2.9418-2.9419 7.7115-2.9419 10.6533 0 2.9419 2.9418 2.9419 7.7115 0 10.6533z' stroke='grey' fill='none' fill-rule='evenodd' stroke-linecap='round' stroke-linejoin='round' %3E%3C/path%3E%3C/svg%3E")
+        background: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='32' height='32' viewBox='0 0 20 20' fill-rule='evenodd'%3E%3Cpath d='M14.386 14.386l4.0877 4.0877-4.0877-4.0877c-2.9418 2.9419-7.7115 2.9419-10.6533 0-2.9419-2.9418-2.9419-7.7115 0-10.6533 2.9418-2.9419 7.7115-2.9419 10.6533 0 2.9419 2.9418 2.9419 7.7115 0 10.6533z' stroke='gray' fill='none' fill-rule='evenodd' stroke-linecap='round' stroke-linejoin='round' %3E%3C/path%3E%3C/svg%3E")
           center / contain no-repeat;
       }
-
       #search-overlay {
         display: none;
         position: fixed;
         z-index: 10;
-        background-color: rgba(0, 0, 0, 0.1);
+        background-color: #1f1f1f0f;
         top: 0;
         left: 0;
         right: 0;
         bottom: 0;
       }
-
       #search-label {
         padding: 0.5rem;
         display: flex;
       }
-
       #search-input {
-        border: 1px solid var(--c-divider-dark, #e5e5e5);
+        border: 1px solid var(--c-border-color);
         border-radius: 0.5rem;
         padding: 0.5rem;
         padding-left: 2.5rem;
         width: 100%;
         z-index: 11;
         position: relative;
-        background: var(--c-bg-light, #fff);
+        background: var(--c-bg-color);
         font-size: large;
         color: inherit;
       }
@@ -230,8 +184,8 @@ class SearchBox extends LitElement {
         list-style: none;
         overflow: auto;
         max-height: 70vh;
-        border: 1px solid var(--c-divider-dark, #e5e5e5);
-        background: var(--c-bg-light, #fff);
+        border: 1px solid var(--c-border-color);
+        background: var(--c-bg-color);
         z-index: 11;
         border-radius: 0.5rem;
         padding: 0.5rem;
@@ -251,7 +205,7 @@ class SearchBox extends LitElement {
         border-color: var(--c-brand);
       }
       #search-hits li {
-        border-bottom: 1px solid var(--c-divider-dark);
+        border-bottom: 1px solid var(--c-border-color);
         margin: 0.5rem 0;
       }
       #search-hits .highlight {
@@ -260,12 +214,12 @@ class SearchBox extends LitElement {
       #search-hits header {
         font-weight: semibold;
       }
-      #search-hits header .section {
+      #search-hits header .tags {
         opacity: 0.7;
         float: right;
       }
-      #search-hits header .section > * {
-        background: #eee;
+      #search-hits header .tags > * {
+        background: var(--c-bg-color);
         padding: 0.125rem 0.5rem;
         border-radius: 0.25rem;
       }
